d36539e4e0a21cab5bb85143dec27a8071e94c06
徐攀 <xupandper@gmail.com>
2016-06-17 22:04:48 +0800

    添加专辑视图

---
+++ iOSDesignPatternDemo/iOSDesignPatternDemo/View/AlbumView.h
@@ -0,0 +1,18 @@
+//
+//  AlbumView.h
+//  iOSDesignPatternDemo
+//
+//  Created by 徐攀 on 16/6/17.
+//  Copyright © 2016年 cn.xupan.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumView : UIView
+
+
+
+- (instancetype)initWithFrame:(CGRect)frame albumCover:(NSString *)albumCover;
+
+
+@end
---
+++ iOSDesignPatternDemo/iOSDesignPatternDemo/View/AlbumView.m
@@ -0,0 +1,50 @@
+//
+//  AlbumView.m
+//  iOSDesignPatternDemo
+//
+//  Created by 徐攀 on 16/6/17.
+//  Copyright © 2016年 cn.xupan.www. All rights reserved.
+//
+
+#import "AlbumView.h"
+
+@interface AlbumView ()
+
+@property (nonatomic, strong) UIImageView *coverImage;
+
+@property (nonatomic, strong) UIActivityIndicatorView *indicator;
+
+@end
+
+@implementation AlbumView
+
+- (UIImageView *)coverImage {
+    if (!_coverImage) {
+        _coverImage = [[UIImageView alloc] initWithFrame:CGRectMake(5, 5, self.frame.size.width - 10, self.frame.size.height - 10)];
+    }
+    return _coverImage;
+}
+
+- (UIActivityIndicatorView *)indicator {
+    if (!_indicator) {
+        // 网络指示器
+        _indicator = [[UIActivityIndicatorView alloc] init];
+        _indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
+        [_indicator startAnimating];
+    }
+    return _indicator;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame albumCover:(NSString *)albumCover {
+    if (self = [super initWithFrame:frame]) {
+        
+        self.backgroundColor = [UIColor blackColor];
+       //   添加封面图
+        [self addSubview:self.coverImage];
+        //  添加下载指示器
+        [self addSubview:self.indicator];
+    }
+    return self;
+}
+
+@end
