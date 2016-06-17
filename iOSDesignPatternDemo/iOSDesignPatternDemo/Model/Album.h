cd6e46b8b08f323d6287a64f036f160c83fac2ff
徐攀 <xupandper@gmail.com>
2016-06-17 21:47:54 +0800

    自定义一个类Album来存储专辑信息

---
+++ iOSDesignPatternDemo/iOSDesignPatternDemo/Model/Album.h
@@ -0,0 +1,26 @@
+//
+//  Album.h
+//  iOSDesignPatternDemo
+//
+//  Created by 徐攀 on 16/6/17.
+//  Copyright © 2016年 cn.xupan.www. All rights reserved.
+//title, *artist, *genre, *coverUrl, *year;
+
+#import <Foundation/Foundation.h>
+
+@interface Album : NSObject
+
+/** 专辑名 */
+@property (nonatomic, copy, readonly) NSString *title;
+/** 艺人 */
+@property (nonatomic, copy, readonly) NSString *artist;
+
+@property (nonatomic, copy, readonly) NSString *genre;
+/** 网址 */
+@property (nonatomic, copy, readonly) NSString *coverUrl;
+/** 发布年 */
+@property (nonatomic, copy, readonly) NSString *year;
+
+- (instancetype)initWithTitle:(NSString *)title artist:(NSString *)artist genre:(NSString *)genre coverUrl:(NSString *)coverUrl year:(NSString *)year;
+
+@end
---
+++ iOSDesignPatternDemo/iOSDesignPatternDemo/Model/Album.m
@@ -0,0 +1,29 @@
+//
+//  Album.m
+//  iOSDesignPatternDemo
+//
+//  Created by 徐攀 on 16/6/17.
+//  Copyright © 2016年 cn.xupan.www. All rights reserved.
+//
+
+#import "Album.h"
+
+@implementation Album
+
+- (instancetype)initWithTitle:(NSString *)title artist:(NSString *)artist
+                        genre:(NSString *)genre coverUrl:(NSString *)coverUrl
+                         year:(NSString *)year {
+    
+    if (self = [super init]) {
+        //注意，只读属性这里不能用点语法，等号左边的点语法实际上是setter方法，而只读属性默认是不生成setter方法的
+        _title = title;
+        _artist = artist;
+        _genre = @"Pop";
+        _coverUrl = coverUrl;
+        _year = year;
+    }
+    
+    return self;
+}
+
+@end
